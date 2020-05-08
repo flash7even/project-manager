310921cc391672e608333bc1870abcf560af6572
tarangokhan <tarangokhan77@gmailcom>
2020-05-08 21:18:32 +0600

    Fix issues with new window

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const Window = require('./Window')
 require('electron-reload')(__dirname)
 
 var submenu_win_height = 800
-var submenu_win_width = 1200
+var submenu_win_width = 1400
 
 let mainWindow
 
@@ -38,8 +38,6 @@ let addBoqWin
 let showBoqsWin
 let showBoqReportsWin
 
-var subwindows = new Array(20);
-
 function createBrowserWindow(sub_window, page_name) {
   if (!sub_window) {
     sub_window = new Window({
@@ -57,6 +55,391 @@ function createBrowserWindow(sub_window, page_name) {
     })
   }
 }
+
+/// -------------- CREATE WINDOWS --------------///
+
+function create_add_project_window(){
+  if (!addProjectWin) {
+    addProjectWin = new Window({
+      file: path.join('src', 'add_project.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showProjectsWin.webContents.openDevTools()
+    addProjectWin.on('closed', () => {
+      addProjectWin = null
+    })
+  }else{
+    addProjectWin.loadFile('src/add_project.html');
+  }
+}
+
+function create_update_project_window(){
+  if (!updateProjectWin) {
+    updateProjectWin = new Window({
+      file: path.join('src', 'update_project.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // updateProjectWin.webContents.openDevTools()
+    updateProjectWin.on('closed', () => {
+      updateProjectWin = null
+    })
+  }else{
+    updateProjectWin.loadFile('src/update_project.html');
+  }
+}
+
+function create_update_show_project_window(){
+  if (!updateShowProjectsWin) {
+    updateShowProjectsWin = new Window({
+      file: path.join('src', 'update_show_projects.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // updateShowProjectsWin.webContents.openDevTools()
+    updateShowProjectsWin.on('closed', () => {
+      updateShowProjectsWin = null
+    })
+  }else{
+    updateShowProjectsWin.loadFile('src/update_show_projects.html');
+  }
+}
+
+function create_show_project_window(){
+  if (!showProjectsWin) {
+    showProjectsWin = new Window({
+      file: path.join('src', 'show_projects.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showProjectsWin.webContents.openDevTools()
+    showProjectsWin.on('closed', () => {
+      showProjectsWin = null
+    })
+  }else{
+    showProjectsWin.loadFile('src/show_projects.html');
+  }
+}
+
+function create_add_transaction_window(){
+  if (!addTransactionWin) {
+    addTransactionWin = new Window({
+      file: path.join('src', 'add_transaction.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // addTransactionWin.webContents.openDevTools()
+    addTransactionWin.on('closed', () => {
+      addTransactionWin = null
+    })
+  }else{
+    addTransactionWin.loadFile('src/add_transaction.html');
+  }
+}
+
+function create_show_transaction_window(){
+  if (!showTransactionsWin) {
+    showTransactionsWin = new Window({
+      file: path.join('src', 'show_transactions.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showProjectsWin.webContents.openDevTools()
+    showTransactionsWin.on('closed', () => {
+      showTransactionsWin = null
+    })
+  }else{
+    showTransactionsWin.loadFile('src/show_transactions.html');
+  }
+}
+
+function create_add_bill_window(){
+  if (!addBillWin) {
+    addBillWin = new Window({
+      file: path.join('src', 'add_bill.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // addBillWin.webContents.openDevTools()
+    addBillWin.on('closed', () => {
+      addBillWin = null
+    })
+  }else{
+    addBillWin.loadFile('src/add_bill.html');
+  }
+}
+
+function create_show_bill_window(){
+  if (!showBillsWin) {
+    showBillsWin = new Window({
+      file: path.join('src', 'show_bills.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showBillsWin.webContents.openDevTools()
+    showBillsWin.on('closed', () => {
+      showBillsWin = null
+    })
+  }else{
+    showBillsWin.loadFile('src/show_bills.html');
+  }
+}
+
+function create_add_boq_window(){
+  if (!addBoqWin) {
+    addBoqWin = new Window({
+      file: path.join('src', 'add_boq.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // addBoqWin.webContents.openDevTools()
+    addBoqWin.on('closed', () => {
+      addBoqWin = null
+    })
+  }else{
+    addBoqWin.loadFile('src/add_boq.html');
+  }
+}
+
+function create_show_boq_list_window(){
+  if (!showBoqsWin) {
+    showBoqsWin = new Window({
+      file: path.join('src', 'show_boqs.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showBoqsWin.webContents.openDevTools()
+    showBoqsWin.on('closed', () => {
+      showBoqsWin = null
+    })
+  }else{
+    showBoqsWin.loadFile('src/show_boqs.html');
+  }
+}
+
+function create_show_boq_report_window(){
+  if (!showBoqReportsWin) {
+    showBoqReportsWin = new Window({
+      file: path.join('src', 'show_boq_reports.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showBoqReportsWin.webContents.openDevTools()
+    showBoqReportsWin.on('closed', () => {
+      showBoqReportsWin = null
+    })
+  }else{
+    showBoqReportsWin.loadFile('src/show_boq_reports.html');
+  }
+}
+
+function create_add_material_window(){
+  if (!addMaterialWin) {
+    addMaterialWin = new Window({
+      file: path.join('src', 'add_material.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // addMaterialWin.webContents.openDevTools()
+    addMaterialWin.on('closed', () => {
+      addMaterialWin = null
+    })
+  }else{
+    addMaterialWin.loadFile('src/add_material.html');
+  }
+}
+
+function create_show_material_window(){
+  if (!showMaterialsWin) {
+    showMaterialsWin = new Window({
+      file: path.join('src', 'show_materials.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showMaterialsWin.webContents.openDevTools()
+    showMaterialsWin.on('closed', () => {
+      showMaterialsWin = null
+    })
+  }else{
+    showMaterialsWin.loadFile('src/show_materials.html');
+  }
+}
+
+function create_add_material_stock_window(){
+  if (!addMaterialStockWin) {
+    addMaterialStockWin = new Window({
+      file: path.join('src', 'add_material_stock.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // addMaterialStockWin.webContents.openDevTools()
+    addMaterialStockWin.on('closed', () => {
+      addMaterialStockWin = null
+    })
+  }else{
+    addMaterialStockWin.loadFile('src/add_material_stock.html');
+  }
+}
+
+function create_show_material_stock_window(){
+  if (!showMaterialStockWin) {
+    showMaterialStockWin = new Window({
+      file: path.join('src', 'show_material_stocks.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showMaterialStockWin.webContents.openDevTools()
+    showMaterialStockWin.on('closed', () => {
+      showMaterialStockWin = null
+    })
+  }else{
+    showMaterialStockWin.loadFile('src/show_material_stocks.html');
+  }
+}
+
+function create_add_payment_method_window(){
+  if (!addPaymentMethodWin) {
+    addPaymentMethodWin = new Window({
+      file: path.join('src', 'add_payment_method.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // addPaymentMethodWin.webContents.openDevTools()
+    addPaymentMethodWin.on('closed', () => {
+      addPaymentMethodWin = null
+    })
+  }else{
+    addPaymentMethodWin.loadFile('src/add_payment_method.html');
+  }
+}
+
+function create_show_payment_method_window(){
+  if (!showPaymentMethodWin) {
+    showPaymentMethodWin = new Window({
+      file: path.join('src', 'show_payment_methods.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showPaymentMethodWin.webContents.openDevTools()
+    showPaymentMethodWin.on('closed', () => {
+      showPaymentMethodWin = null
+    })
+  }else{
+    showPaymentMethodWin.loadFile('src/show_payment_methods.html');
+  }
+}
+
+function create_show_balance_sheet_window(){
+  if (!showBalanceSheetWin) {
+    showBalanceSheetWin = new Window({
+      file: path.join('src', 'show_balance_sheets.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showBalanceSheetWin.webContents.openDevTools()
+    showBalanceSheetWin.on('closed', () => {
+      showBalanceSheetWin = null
+    })
+  }else{
+    showBalanceSheetWin.loadFile('src/show_balance_sheets.html');
+  }
+}
+
+function create_show_graph_window(){
+  if (!showGraphWin) {
+    showGraphWin = new Window({
+      file: path.join('src', 'index.html'),
+      width: submenu_win_width,
+      height: submenu_win_height,
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    // showGraphWin.webContents.openDevTools()
+    showGraphWin.on('closed', () => {
+      showGraphWin = null
+    })
+  }else{
+    showGraphWin.loadFile('src/index.html');
+  }
+}
+
+
+/// -------------- CREATE WINDOWS --------------///
 
 
 function main () {
@@ -91,19 +474,19 @@ function main () {
         {
           label:'Add Project',
           click() {
-            createBrowserWindow(addProjectWin, 'add_project.html')
+            create_add_project_window();
           }
         },
         {
           label:'Update Project',
           click() {
-            createBrowserWindow(updateShowProjectsWin, 'update_show_projects.html')
+            create_update_show_project_window()
           }
         },
         {
           label:'Project Report',
           click() {
-            createBrowserWindow(showProjectsWin, 'show_projects.html')
+            create_show_project_window();
           }
         }
       ]
@@ -114,7 +497,7 @@ function main () {
         {
           label:'Add Transaction',
           click() {
-            createBrowserWindow(addTransactionWin, 'add_transaction.html')
+            create_add_transaction_window();
           }
         }
       ]
@@ -125,7 +508,7 @@ function main () {
         {
           label:'Add Bill',
           click() {
-            createBrowserWindow(addBillWin, 'add_bill.html')
+            create_add_bill_window();
           }
         },
       ]
@@ -137,25 +520,25 @@ function main () {
         {
           label:'Add Material',
           click() {
-            createBrowserWindow(addMaterialWin, 'add_material.html')
+            create_add_material_window();
           }
         },
         {
           label:'Add Material Stock',
           click() {
-            createBrowserWindow(addMaterialStockWin, 'add_material_stock.html')
+            create_add_material_stock_window();
           }
         },
         {
           label:'Material List',
           click() {
-            createBrowserWindow(showMaterialsWin, 'show_materials.html')
+            create_show_material_window();
           }
         },
         {
           label:'Material Stock List',
           click() {
-            createBrowserWindow(showMaterialStockWin, 'show_material_stocks.html')
+            create_show_material_stock_window();
           }
         }
       ]
@@ -166,13 +549,13 @@ function main () {
         {
           label:'Add BOQ',
           click() {
-            createBrowserWindow(addBoqWin, 'add_boq.html')
+            create_add_boq_window();
           }
         },
         {
-          label:'Boq List',
+          label:'BOQ List',
           click() {
-            createBrowserWindow(showBoqsWin, 'show_boqs.html')
+            create_show_boq_list_window();
           }
         }
       ]
@@ -183,13 +566,13 @@ function main () {
         {
           label:'Add Payment Method',
           click() {
-            createBrowserWindow(addPaymentMethodWin, 'add_payment_method.html')
+            create_add_payment_method_window();
           }
         },
         {
           label:'Payment Method List',
           click() {
-            createBrowserWindow(showPaymentMethodWin, 'show_payment_methods.html')
+            create_show_payment_method_window();
           }
         }
       ]
@@ -200,31 +583,31 @@ function main () {
         {
           label:'Transaction Report',
           click() {
-            createBrowserWindow(showTransactionsWin, 'show_transactions.html')
+            create_show_transaction_window();
           }
         },
         {
           label:'Bill Report',
           click() {
-            createBrowserWindow(showBillsWin, 'show_bills.html')
+            create_show_bill_window();
           }
         },
         {
-          label:'Boq Reports',
+          label:'BOQ Reports',
           click() {
-            createBrowserWindow(showBoqReportsWin, 'show_boq_reports.html')
+            create_show_boq_report_window();
           }
         },
         {
           label:'Balance Sheet',
           click() {
-            createBrowserWindow(showBalanceSheetWin, 'show_balance_sheets.html')
+            create_show_balance_sheet_window();
           }
         },
         {
           label:'Graphs',
           click() {
-            createBrowserWindow(showGraphWin, 'index.html')
+            create_show_graph_window();
           }
         }
       ]
@@ -234,8 +617,7 @@ function main () {
 }
 
 ipcMain.on('after-transaction', (event, message) => {
-  addTransactionWin = null;
-  createBrowserWindow(showTransactionsWin, 'show_transactions.html')
+  create_show_transaction_window();
 })
 
 ipcMain.on('after-transaction-update', (event, message) => {
@@ -246,33 +628,27 @@ ipcMain.on('after-transaction-update', (event, message) => {
 })
 
 ipcMain.on('after-project-creation', (event, message) => {
-  addProjectWin = null;
-  createBrowserWindow(showProjectsWin, 'show_projects.html')
+  create_show_project_window();
 })
 
 ipcMain.on('after-payment-method-creation', (event, message) => {
-  addPaymentMethodWin = null;
-  createBrowserWindow(showPaymentMethodWin, 'show_payment_methods.html')
+  create_show_payment_method_window();
 })
 
 ipcMain.on('after-project-update', (event, message) => {
-  updateProjectWin = null;
-  createBrowserWindow(showProjectsWin, 'show_projects.html')
+  create_show_project_window();
 })
 
 ipcMain.on('after-bill', (event, message) => {
-  addBillWin = null;
-  createBrowserWindow(showBillsWin, 'show_bills.html')
+  create_show_bill_window();
 })
 
 ipcMain.on('after-material', (event, message) => {
-  addMaterialWin = null;
-  createBrowserWindow(showMaterialsWin, 'show_materials.html')
+  create_show_material_window();
 })
 
 ipcMain.on('after-material-stock', (event, message) => {
-  addMaterialStockWin = null;
-  createBrowserWindow(showMaterialStockWin, 'show_material_stocks.html')
+  create_show_material_stock_window();
 })
 
 ipcMain.on('after-material-update', (event, message) => {
@@ -283,8 +659,7 @@ ipcMain.on('after-material-update', (event, message) => {
 })
 
 ipcMain.on('after-boq', (event, message) => {
-  addBoqWin = null;
-  createBrowserWindow(showBoqsWin, 'show_boqs.html')
+  create_show_boq_list_window();
 })
 
 ipcMain.on('after-boq-update', (event, message) => {
@@ -295,7 +670,7 @@ ipcMain.on('after-boq-update', (event, message) => {
 })
 
 ipcMain.on('call-project-update', (event, message) => {
-  createBrowserWindow(updateProjectWin, 'update_project.html')
+  create_update_project_window();
 })
 
 ipcMain.on('call-material-update', (event, message) => {
